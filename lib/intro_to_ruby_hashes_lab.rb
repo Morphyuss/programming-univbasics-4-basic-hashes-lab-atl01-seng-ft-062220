eea0293c4e6ce879aa707fa6ba0d014e95e9d22d
James Stamps Jr <jstampshere@gmail.com>
2020-04-06 19:34:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def new_hash
-  # return an empty hash
+ new_hash = Hash.new # return an empty hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ my_hash {
+   
+ } # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
